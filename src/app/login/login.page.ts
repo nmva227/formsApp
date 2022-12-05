7d91f3c2a247c07fbe51d4b437502b41dad3ba0f
Nickolas Passos Homem de Mello <88170277+MelloNickolas@users.noreply.github.com>
2022-12-05 09:43:34 -0300

    Começo da Validação

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formRegistro: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formRegistro = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cpf: ['',  Validators.compose([Validators.required, Validators.minLength(11)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmaSenha: ['',  Validators.compose([Validators.required, Validators.minLength(6)])]
+
+    });
+    // o minLength, serve para colocar o minimos de dados para vocÊ validar, exemplo
+    //coloque no minimo 3 letras ou nao vai funcionar e o mesmo vale com o maxLenght
+   }
 
   ngOnInit() {
   }
