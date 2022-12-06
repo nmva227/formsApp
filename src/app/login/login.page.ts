6e4970239ed501dbbbc9d7db318297f796ce2b82
Nickolas Passos Homem de Mello <88170277+MelloNickolas@users.noreply.github.com>
2022-12-06 09:47:10 -0300

    Começar a armazenar

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,22 +7,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
 
-  formRegistro: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.formRegistro = this.formBuilder.group({
-      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      cpf: ['',  Validators.compose([Validators.required, Validators.minLength(11)])],
-      email: ['', Validators.compose([Validators.required, Validators.email])],
-      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      confirmaSenha: ['',  Validators.compose([Validators.required, Validators.minLength(6)])]
-
-    });
-    // o minLength, serve para colocar o minimos de dados para vocÊ validar, exemplo
-    //coloque no minimo 3 letras ou nao vai funcionar e o mesmo vale com o maxLenght
-   }
+  constructor() {}
 
   ngOnInit() {
   }
 
 }
+
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,7 @@
+import { comparaValidator } from './../validators/compara-validator';
+import { CpfValidator } from './../validators/cpf-validator';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formRegistro: FormGroup;
+
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'O campo nome é obrigatório' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' }
+    ],
+    cpf: [
+      { tipo: 'required', mensagem: 'O campo CPF é obrigatório.' },
+      { tipo: 'invalido', mensagem: 'CPF Inválido.' },
+    ],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+    confirmaSenha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a Senha.' },
+    ],
+  };
+
+  constructor(private formBuilder: FormBuilder) { this.formRegistro = this.formBuilder.group({
+    nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    cpf: ['',  Validators.compose([Validators.required, CpfValidator.cpfValido])],
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    senha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+    confirmaSenha: ['',  Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])]
+  }, {
+    validator : comparaValidator('senha', 'confirmaSenha')
+  });
+  // o minLength, serve para colocar o minimos de dados para vocÊ validar, exemplo
+  //coloque no minimo 3 letras ou nao vai funcionar e o mesmo vale com o maxLenght
+  }
 
   ngOnInit() {
+  }
+
+  salvarRegistro(){
+    console.log('Formulário: ', this.formRegistro.valid);
   }
 
 }
